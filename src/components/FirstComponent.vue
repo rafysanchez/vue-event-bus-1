acfafdb289ca4191595d8cf6621e704925dc7361
rafael sanchez <rafysanchez@hotmail.com>
2024-01-22 15:33:42 -0300

    init

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data: function () {
+    return {
+      testEvent: 'String to change'
+    }
+  },
+  created (){
+    this.emitter.on('my-event', (evt) => {
+      this.testEvent = evt.eventContent;
+    })
+  }
+}
+</script>
+
+<template>
+  <div style="border: 1px solid black;">
+    <h1>First Component</h1>
+    <p>{{testEvent}}</p>
+  </div>
+</template>
+
+<style scoped>
+div{
+  border-radius: 16px;
+  background-color: orange;
+}
+p{
+  font-weight: 900;
+}
+a {
+  color: #42b983;
+}
+
+</style>
---
+++ src/components/SecondComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div style="border: 1px solid black;">
+        <h1>Second Component</h1>
+        <button v-on:click="emitMyEvent">Emit Event</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        data: function () {
+    return {
+      contar: Number = 1
+    }
+  },
+        methods: {
+          emitMyEvent() {
+              this.emitter.emit('my-event', {'eventContent': 'String changed: ' + this.contar ++})
+          }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
